5dccbdd769c71da637c2a0fb3e4de2425be673d8
Mikael Kvalvaer <mikaelkvalvaer@gmail.com>
2022-08-04 18:07:19 +0200

    fix 404 page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,4 +1,5 @@
-<div class="flex h-[92vh] mx-auto w-screen flex-col bg-[#f7f7f7] items-center">
+<!-- <div class="flex h-[92vh] mx-auto w-screen flex-col bg-[#f7f7f7] items-center"> -->
+<div class="bg-[#f7f7f7] flex flex-col px-4 items-center min-h-screen py-0 my-0 mx-auto">
     <p class="text-4xl font-bold mt-12">404</p>
     <p class="text-xl font-bold mt-4">Looks like the server got too much coffee</p>
     <p class="text-sm font-bold mt-2">... and broke the link</p>
